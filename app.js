6c64801c002b6b042b804593acddf4a27e82cfbe
affamd109 <affaque109@gmail.com>
2025-03-20 23:37:56 +0530

    delte_updates

--- app.js
+++ app.js
@@ -119,7 +119,10 @@ app.post('/campgrounds/:id/reviews', validateReview , catchAsync(async (req, res
 
 
 app.delete('/campgrounds/:id/reviews/:reviewId' , catchAsync( async (req , res) =>{
-    res.send('Delete me !!!');
+    const {id , reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id , {$pull : {reviews : reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all(/(.*)/, (req, res, next) => {
