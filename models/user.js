3680f29bc993852e90bc48de1f49674a6f685ef8
affamd109 <affaque109@gmail.com>
2025-03-30 19:05:45 +0530

    passport

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+//yes i only need to write meail cuz of the below plugin
+//The below plugin is used to give fileds like username and passport by  default!
+//Passport-local
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
