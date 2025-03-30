2e4e4e0422ebd0c0f6cc411768b7631e45f95f0e
affamd109 <affaque109@gmail.com>
2025-03-31 02:46:33 +0530

    auth2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
+
 
 router.get('/register' , (req , res) =>{
     res.render('users/register');
@@ -16,13 +18,23 @@ router.post('/register' , catchAsync(async (req , res) =>{
         console.log(registeredUser);
         req.flash( 'success' , 'Welcome to CampQuest');
         res.redirect("/campgrounds");
-
     }
     catch(e){
         req.flash('error' , e.message);
         res.redirect('/register');
     }
 }));
+
+router.get('/login' , (req , res) =>{
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
 
 
 
