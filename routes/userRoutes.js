72786970526f644d9d3fef2907b22b641e486b00
affamd109 <affaque109@gmail.com>
2025-04-03 02:47:11 +0530

    logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -37,5 +37,15 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 })
 
 
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        // req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
+
 
 module.exports = router;
