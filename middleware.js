4580f3e050202813925538b49844d1a41bdb0154
affamd109 <affaque109@gmail.com>
2025-04-03 13:26:08 +0530

    camps_page

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 module.exports.isLoggedIn = (req , res , next) =>{
+    console.log('Req.User : ' , req.user);
     if(!req.isAuthenticated()){
-        req.flash('error' , 'You must be signed in ');
+        req.flash('error' , 'You must be signed in! ');
         return res.redirect('/login');
     }
     next();
