18da8dedf7c448948f51ea7a038c6262917bf379
affamd109 <affaque109@gmail.com>
2025-05-10 19:39:40 +0530

    loginISsue

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('trust proxy', 1);
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
